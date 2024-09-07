367f1962f2c7a02ce967de30c90dcdd7d2e97e5f
rohim48 <noerrohim10@gmail.com>
2024-09-07 14:37:08 +0700

    finish about & service pages

---
+++ js/main.js
@@ -0,0 +1,5 @@
+AOS.init({
+    duration: 1000, // Durasi animasi dalam milidetik
+    offset: 200,    // Jarak offset sebelum animasi dipicu
+    once: true,     // Animasi terjadi hanya sekali saat elemen muncul di layar
+  });
