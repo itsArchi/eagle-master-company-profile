76d7a9eeef0697f12dbdfd6d77412744be35f815
rohim48 <noerrohim10@gmail.com>
2024-09-08 09:07:13 +0700

    add dark mode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,49 @@
 AOS.init({
-    duration: 1000, // Durasi animasi dalam milidetik
-    offset: 200,    // Jarak offset sebelum animasi dipicu
-    once: true,     // Animasi terjadi hanya sekali saat elemen muncul di layar
+  duration: 1000,
+  offset: 200,
+  once: true,
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggle-dark-mode");
+  const body = document.body;
+
+  if (localStorage.getItem("dark-mode") === "enabled") {
+    body.classList.add("dark-mode");
+    //   toggleButton.textContent = 'Light Mode';
+  } else {
+    body.classList.remove("dark-mode");
+    //   toggleButton.textContent = 'Dark Mode';
+  }
+
+  toggleButton.addEventListener("click", function () {
+    if (body.classList.contains("dark-mode")) {
+      body.classList.remove("dark-mode");
+      //   toggleButton.textContent = 'Dark Mode';
+      localStorage.setItem("dark-mode", "disabled");
+    } else {
+      body.classList.add("dark-mode");
+      //   toggleButton.textContent = 'Light Mode';
+      localStorage.setItem("dark-mode", "enabled");
+    }
   });
+});
+
+const goToTopButton = document.getElementById("go-to-top");
+
+function toggleGoToTopButton() {
+  if (window.scrollY > 300) {
+    goToTopButton.style.display = "flex";
+  } else {
+    goToTopButton.style.display = "none";
+  }
+}
+
+window.addEventListener("scroll", toggleGoToTopButton);
+
+goToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
